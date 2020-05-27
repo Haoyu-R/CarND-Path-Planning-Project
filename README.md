89b65a238a3387a59af8656fac80881649b53c1a
Haoyu <arhyrhy185@gmail.com>
2020-05-27 20:09:35 +0200

    Finish the project

--- README.md
+++ README.md
@@ -92,54 +92,154 @@ A really helpful resource for doing this project and creating smooth trajectorie
     git checkout e94b6e1
     ```
 
-## Editor Settings
+## Code Overview
 
-We've purposefully kept editor configuration files out of this repo in order to
-keep it as simple and environment agnostic as possible. However, we recommend
-using the following settings:
+1. Given a lane number, a distance "horizon" to look forward and a distance "buffer" to look backward, Check whether in a vehicle is too close to ego vehicle. 
+```
+bool check_too_close(const vector<vector<double>>& sensor_fusion, const int& lane, const double& car_s, const int& prev_size, int horizon, int buffer) {
 
-* indent using spaces
-* set tab width to 2 spaces (keeps the matrices in source code aligned)
+    for (size_t i = 0; i < sensor_fusion.size(); i++) {
 
-## Code Style
+        float d = sensor_fusion[i][6];
+        if (d < (2 + 4 * lane + 2) && d >(2 + 4 * lane - 2)) {
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double check_speed = sqrt(vx * vx + vy * vy);
+            double check_car_s = sensor_fusion[i][5];
+            // project the car s coordinates out to end of prev list
+            check_car_s += (double)prev_size * 0.02 * check_speed;
 
-Please (do your best to) stick to [Google's C++ style guide](https://google.github.io/styleguide/cppguide.html).
+            if ((check_car_s > car_s - buffer) && (check_car_s - horizon < car_s)) {
+                return true;
+            }
+        }
+    }
 
-## Project Instructions and Rubric
+    return false;
+}
+```
 
-Note: regardless of the changes you make, your project must be buildable using
-cmake and make!
+2. Check is ego vehicle is too close to a front vehicle, if yes, whether a lane change is possible
+```
+bool too_close = false;
+// Check if a car is in front of us
+too_close = check_too_close(sensor_fusion, lane, car_s, prev_size, 30, 0);
+// Check if lane change is possible
+if (too_close) {
+    if (lane == 0) {
+        if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 35, 5)) {
+            lane++;
+        }
+        // Check if a pass at right most lane is possible 
+        else if (!check_too_close(sensor_fusion, lane + 2, car_s, prev_size, 35, 5)) {
+            if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 20, 5)) {
+                lane++;
+            }
+        }
+    }
+    else if (lane == 1) {
+        if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 25, 5)) {
+            lane--;
+        }
+        else if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 35, 5)) {
+            lane++;
+        }
+    }
+    else {
+        if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 25, 5)) {
+            lane--;
+        }// Check if a pass at left most lane is possible 
+        else if (!check_too_close(sensor_fusion, lane - 2, car_s, prev_size, 35, 5)) {
+            if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 20, 5)) {
+                lane--;
+            }
+        }
+    }
+}
+```
 
+3. Using some reference points to creat a set of points for spline (trajectory generation)
+```
+if (prev_size < 2) {
+    double prev_car_x = car_x - cos(car_yaw);
+    double prev_car_y = car_y - sin(car_yaw);
 
-## Call for IDE Profiles Pull Requests
+    ptsx.push_back(prev_car_x);
+    ptsx.push_back(car_x);
 
-Help your fellow students!
+    ptsy.push_back(prev_car_y);
+    ptsy.push_back(car_y);
+}
+else {
+    ref_x = previous_path_x[prev_size - 1];
+    ref_y = previous_path_y[prev_size - 1];
 
-We decided to create Makefiles with cmake to keep this project as platform
-agnostic as possible. Similarly, we omitted IDE profiles in order to ensure
-that students don't feel pressured to use one IDE or another.
+    double ref_x_prev = previous_path_x[prev_size - 2];
+    double ref_y_prev = previous_path_y[prev_size - 2];
+    ptsx.push_back(ref_x_prev);
+    ptsx.push_back(ref_x);
 
-However! I'd love to help people get up and running with their IDEs of choice.
-If you've created a profile for an IDE that you think other students would
-appreciate, we'd love to have you add the requisite profile files and
-instructions to ide_profiles/. For example if you wanted to add a VS Code
-profile, you'd add:
+    ptsy.push_back(ref_y_prev);
+    ptsy.push_back(ref_y);
+    ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
 
-* /ide_profiles/vscode/.vscode
-* /ide_profiles/vscode/README.md
+}
 
-The README should explain what the profile does, how to take advantage of it,
-and how to install it.
+vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+```
 
-Frankly, I've never been involved in a project with multiple IDE profiles
-before. I believe the best way to handle this would be to keep them out of the
-repo root to avoid clutter. My expectation is that most profiles will include
-instructions to copy files to a new location to get picked up by the IDE, but
-that's just a guess.
+4. Transfer the reference points from world coordinate to ego coordinate and put them to spline
+```
+// convert origin reference coordinates to ego in current frame
+for (size_t i = 0; i < ptsx.size(); i++) {
+double shift_x = ptsx[i] - ref_x;
+double shift_y = ptsy[i] - ref_y;
 
-One last note here: regardless of the IDE used, every submitted project must
-still be compilable with cmake and make./
+ptsx[i] = (shift_x * cos(0 - ref_yaw)) - shift_y * sin(0 - ref_yaw);
+ptsy[i] = (shift_x * sin(0 - ref_yaw)) + shift_y * cos(0 - ref_yaw);
+}
+// set spline
+tk::spline s;
+s.set_points(ptsx, ptsy);
+```
 
-## How to write a README
-A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
+5. Fill the trajectory up to 50 points with the points generated by spline.
+```
+// some horizon value in front of ego car
+double target_x = 30.0;
+double target_y = s(target_x);
+double target_dist = sqrt(target_x * target_x + target_y * target_y);
 
+double x_add_on = 0;
+
+for (size_t i = 0; i < 50 - prev_size; i++) {
+    // If it is too close to a front a vehicle, we slow down
+    if (too_close) {
+        ref_vel -= 0.15;
+    } // If not we speed up to limit
+    else if (ref_vel < 49.6) {
+        ref_vel += 0.2;
+    }
+    //linearalization
+    double N = target_dist / (0.02 * ref_vel / 2.24);
+    double x_point = x_add_on + target_x / N;
+    double y_point = s(x_point);
+
+    x_add_on = x_point;
+
+    double x_ref = x_point;
+    double y_ref = y_point;
+
+    //transfer back to global coordinates
+    x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+    y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+    x_point += ref_x;
+    y_point += ref_y;
+
+    next_x_vals.push_back(x_point);
+    next_y_vals.push_back(y_point);
+}
+```
--- src/main.cpp
+++ src/main.cpp
@@ -13,124 +13,302 @@ using nlohmann::json;
 using std::string;
 using std::vector;
 
+bool check_too_close(const vector<vector<double>>& sensor_fusion, const int& lane, const double& car_s, const int& prev_size, int horizon, int buffer) {
+
+    for (size_t i = 0; i < sensor_fusion.size(); i++) {
+
+        float d = sensor_fusion[i][6];
+        if (d < (2 + 4 * lane + 2) && d >(2 + 4 * lane - 2)) {
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double check_speed = sqrt(vx * vx + vy * vy);
+            double check_car_s = sensor_fusion[i][5];
+            // project the car s coordinates out to end of prev list
+            check_car_s += (double)prev_size * 0.02 * check_speed;
+
+            if ((check_car_s > car_s - buffer) && (check_car_s - horizon < car_s)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 int main() {
-  uWS::Hub h;
+    uWS::Hub h;
 
-  // Load up map values for waypoint's x,y,s and d normalized normal vectors
-  vector<double> map_waypoints_x;
-  vector<double> map_waypoints_y;
-  vector<double> map_waypoints_s;
-  vector<double> map_waypoints_dx;
-  vector<double> map_waypoints_dy;
+    // Load up map values for waypoint's x,y,s and d normalized normal vectors
+    vector<double> map_waypoints_x;
+    vector<double> map_waypoints_y;
+    vector<double> map_waypoints_s;
+    vector<double> map_waypoints_dx;
+    vector<double> map_waypoints_dy;
 
-  // Waypoint map to read from
-  string map_file_ = "../data/highway_map.csv";
-  // The max s value before wrapping around the track back to 0
-  double max_s = 6945.554;
+    // Waypoint map to read from
+    string map_file_ = "../data/highway_map.csv";
+    // The max s value before wrapping around the track back to 0
+    double max_s = 6945.554;
 
-  std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
+    std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
-  string line;
-  while (getline(in_map_, line)) {
-    std::istringstream iss(line);
-    double x;
-    double y;
-    float s;
-    float d_x;
-    float d_y;
-    iss >> x;
-    iss >> y;
-    iss >> s;
-    iss >> d_x;
-    iss >> d_y;
-    map_waypoints_x.push_back(x);
-    map_waypoints_y.push_back(y);
-    map_waypoints_s.push_back(s);
-    map_waypoints_dx.push_back(d_x);
-    map_waypoints_dy.push_back(d_y);
-  }
+    string line;
+    while (getline(in_map_, line)) {
+        std::istringstream iss(line);
+        double x;
+        double y;
+        float s;
+        float d_x;
+        float d_y;
+        iss >> x;
+        iss >> y;
+        iss >> s;
+        iss >> d_x;
+        iss >> d_y;
+        map_waypoints_x.push_back(x);
+        map_waypoints_y.push_back(y);
+        map_waypoints_s.push_back(s);
+        map_waypoints_dx.push_back(d_x);
+        map_waypoints_dy.push_back(d_y);
+    }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
-              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-               uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+    // reference speed
+    double ref_vel = 0;
+    // current lane
+    int lane = 1;
+    h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s,
+        &map_waypoints_dx, &map_waypoints_dy, &ref_vel, &lane]
+        (uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
+            uWS::OpCode opCode) {
+        // "42" at the start of the message means there's a websocket message event.
+        // The 4 signifies a websocket message
+        // The 2 signifies a websocket event
+        if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
-      auto s = hasData(data);
+            auto s = hasData(data);
 
-      if (s != "") {
-        auto j = json::parse(s);
-        
-        string event = j[0].get<string>();
-        
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          
-          // Main car's localization Data
-          double car_x = j[1]["x"];
-          double car_y = j[1]["y"];
-          double car_s = j[1]["s"];
-          double car_d = j[1]["d"];
-          double car_yaw = j[1]["yaw"];
-          double car_speed = j[1]["speed"];
+            if (s != "") {
+                auto j = json::parse(s);
 
-          // Previous path data given to the Planner
-          auto previous_path_x = j[1]["previous_path_x"];
-          auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
-          double end_path_s = j[1]["end_path_s"];
-          double end_path_d = j[1]["end_path_d"];
+                string event = j[0].get<string>();
 
-          // Sensor Fusion Data, a list of all other cars on the same side 
-          //   of the road.
-          auto sensor_fusion = j[1]["sensor_fusion"];
+                if (event == "telemetry") {
+                    // j[1] is the data JSON object
 
-          json msgJson;
+                    // Main car's localization Data
+                    double car_x = j[1]["x"];
+                    double car_y = j[1]["y"];
+                    double car_s = j[1]["s"];
+                    double car_d = j[1]["d"];
+                    double car_yaw = j[1]["yaw"];
+                    double car_speed = j[1]["speed"];
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
+                    // Previous path data given to the Planner
+                    auto previous_path_x = j[1]["previous_path_x"];
+                    auto previous_path_y = j[1]["previous_path_y"];
+                    // Previous path's end s and d values 
+                    double end_path_s = j[1]["end_path_s"];
+                    double end_path_d = j[1]["end_path_d"];
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+                    // Sensor Fusion Data, a list of all other cars on the same side 
+                    //   of the road.
+                    auto sensor_fusion = j[1]["sensor_fusion"];
+
+                    json msgJson;
+
+                    // the way points that the simulator hasn't explored from last time
+                    int prev_size = previous_path_x.size();
+
+                    if (prev_size > 0) {
+                        car_s = end_path_s;
+                    }
+
+                    bool too_close = false;
+                    // Check if a car is in front of us
+                    too_close = check_too_close(sensor_fusion, lane, car_s, prev_size, 30, 0);
+                    // Check if lane change is possible
+                    if (too_close) {
+                        if (lane == 0) {
+                            if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 35, 5)) {
+                                lane++;
+                            }
+                            // Check if a pass at right most lane is possible 
+                            else if (!check_too_close(sensor_fusion, lane + 2, car_s, prev_size, 35, 5)) {
+                                if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 20, 5)) {
+                                    lane++;
+                                }
+                            }
+                        }
+                        else if (lane == 1) {
+                            if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 25, 5)) {
+                                lane--;
+                            }
+                            else if (!check_too_close(sensor_fusion, lane + 1, car_s, prev_size, 35, 5)) {
+                                lane++;
+                            }
+                        }
+                        else {
+                            if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 25, 5)) {
+                                lane--;
+                            }// Check if a pass at left most lane is possible 
+                            else if (!check_too_close(sensor_fusion, lane - 2, car_s, prev_size, 35, 5)) {
+                                if (!check_too_close(sensor_fusion, lane - 1, car_s, prev_size, 20, 5)) {
+                                    lane--;
+                                }
+                            }
+                        }
+                    }
+                    // creat set of points for spline
+                    vector<double> ptsx;
+                    vector<double> ptsy;
+
+                    double ref_x = car_x;
+                    double ref_y = car_y;
+                    double ref_yaw = deg2rad(car_yaw);
+
+                    if (prev_size < 2) {
+                        double prev_car_x = car_x - cos(car_yaw);
+                        double prev_car_y = car_y - sin(car_yaw);
+
+                        ptsx.push_back(prev_car_x);
+                        ptsx.push_back(car_x);
+
+                        ptsy.push_back(prev_car_y);
+                        ptsy.push_back(car_y);
+                    }
+                    else {
+                        ref_x = previous_path_x[prev_size - 1];
+                        ref_y = previous_path_y[prev_size - 1];
+
+                        double ref_x_prev = previous_path_x[prev_size - 2];
+                        double ref_y_prev = previous_path_y[prev_size - 2];
+                        ptsx.push_back(ref_x_prev);
+                        ptsx.push_back(ref_x);
+
+                        ptsy.push_back(ref_y_prev);
+                        ptsy.push_back(ref_y);
+                        ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+                    }
+
+                    vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+                    ptsx.push_back(next_wp0[0]);
+                    ptsx.push_back(next_wp1[0]);
+                    ptsx.push_back(next_wp2[0]);
+
+                    ptsy.push_back(next_wp0[1]);
+                    ptsy.push_back(next_wp1[1]);
+                    ptsy.push_back(next_wp2[1]);
+
+                    // convert origin reference coordinates to ego in current frame
+                    for (size_t i = 0; i < ptsx.size(); i++) {
+                        double shift_x = ptsx[i] - ref_x;
+                        double shift_y = ptsy[i] - ref_y;
+
+                        ptsx[i] = (shift_x * cos(0 - ref_yaw)) - shift_y * sin(0 - ref_yaw);
+                        ptsy[i] = (shift_x * sin(0 - ref_yaw)) + shift_y * cos(0 - ref_yaw);
+                    }
+                    // set spline
+                    tk::spline s;
+                    s.set_points(ptsx, ptsy);
+                    // recover the points that havn't been use by simulator
+
+                    vector<double> next_x_vals;
+                    vector<double> next_y_vals;
 
 
-          msgJson["next_x"] = next_x_vals;
-          msgJson["next_y"] = next_y_vals;
+                    for (size_t i = 0; i < previous_path_x.size(); i++) {
+                        next_x_vals.push_back(previous_path_x[i]);
+                        next_y_vals.push_back(previous_path_y[i]);
+                    }
+                    // some horizon value in front of ego car
+                    double target_x = 30.0;
+                    double target_y = s(target_x);
+                    double target_dist = sqrt(target_x * target_x + target_y * target_y);
 
-          auto msg = "42[\"control\","+ msgJson.dump()+"]";
+                    double x_add_on = 0;
 
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }  // end websocket if
-  }); // end h.onMessage
+                    for (size_t i = 0; i < 50 - prev_size; i++) {
+                        // If it is too close to a front a vehicle, we slow down
+                        if (too_close) {
+                            ref_vel -= 0.15;
+                        } // If not we speed up to limit
+                        else if (ref_vel < 49.6) {
+                            ref_vel += 0.2;
+                        }
+                        //linearalization
+                        double N = target_dist / (0.02 * ref_vel / 2.24);
+                        double x_point = x_add_on + target_x / N;
+                        double y_point = s(x_point);
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+                        x_add_on = x_point;
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+                        double x_ref = x_point;
+                        double y_ref = y_point;
 
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
-  } else {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  
-  h.run();
+                        //transfer back to global coordinates
+                        x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+                        y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+                        x_point += ref_x;
+                        y_point += ref_y;
+
+                        next_x_vals.push_back(x_point);
+                        next_y_vals.push_back(y_point);
+                    }
+
+                    /**
+                     * TODO: define a path made up of (x,y) points that the car will visit
+                     *   sequentially every .02 seconds
+                     */
+                     // double dist_inc = 0.5;
+                     // for (int i = 0; i < 50; ++i) {
+                     //   double next_s = car_s + (i+1)*dist_inc;
+
+                     //   double next_d = car_d;
+                     //   vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+                     //   next_x_vals.push_back(xy[0]);
+                     //   next_y_vals.push_back(xy[1]);
+                     // }
+
+                    msgJson["next_x"] = next_x_vals;
+                    msgJson["next_y"] = next_y_vals;
+
+                    auto msg = "42[\"control\"," + msgJson.dump() + "]";
+
+                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                }  // end "telemetry" if
+            }
+            else {
+                // Manual driving
+                std::string msg = "42[\"manual\",{}]";
+                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            }
+        }  // end websocket if
+    }); // end h.onMessage
+
+    h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+        std::cout << "Connected!!!" << std::endl;
+    });
+
+    h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+        char* message, size_t length) {
+        ws.close();
+        std::cout << "Disconnected" << std::endl;
+    });
+
+    int port = 4567;
+    if (h.listen(port)) {
+        std::cout << "Listening to port " << port << std::endl;
+    }
+    else {
+        std::cerr << "Failed to listen to port" << std::endl;
+        return -1;
+    }
+
+    h.run();
 }
